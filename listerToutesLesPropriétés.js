8cfa21d44ed0bbc689be2e0f5debc30b87bc9bdd
Van Dravik <jan@vandravik.com>
2020-04-10 12:11:45 +0200

    Create listerToutesLesPropriétés.js

---
+++ listerToutesLesPropriétés.js
@@ -0,0 +1,13 @@
+// Parcourir les propriétés d'un objet
+
+function listerToutesLesPropriétés(o){
+  var objectToInspect;
+  var result = [];
+  
+  for(objectToInspect = o;
+      objectToInspect !== null;
+      objectToInspect = Object.getPrototypeOf(objectToInspect)){  
+    result = result.concat(Object.getOwnPropertyNames(objectToInspect));  
+  }
+  return result; 
+}
